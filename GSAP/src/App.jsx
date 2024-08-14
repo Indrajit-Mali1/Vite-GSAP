69768d0950121394eedcd6778f8de3dae8eacf03
Indrajit Mali <142807827+Indrajit-Mali1@users.noreply.github.com>
2024-08-14 02:39:41 +0000

    modified:   GSAP/src/App.jsx

--- GSAP/src/App.jsx
+++ GSAP/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import gsap from "gsap";
+import {useGSAP} from "@gsap/react";
 
 function App() {
   const [count, setCount] = useState(0)
-
+  useGSAP(()=>{
+    gsap.from()
+  })
+  
 
   return (
     <>
