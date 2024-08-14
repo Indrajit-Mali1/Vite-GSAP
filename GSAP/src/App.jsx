46cf08ff15e1769fbe1e2334413d431f4d3e353f
Indrajit Mali <142807827+Indrajit-Mali1@users.noreply.github.com>
2024-08-14 02:57:54 +0000

    modified:   GSAP/src/App.jsx

--- GSAP/src/App.jsx
+++ GSAP/src/App.jsx
@@ -3,19 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import gsap from "gsap";
-import {useGSAP} from "@gsap/react";
+// import { useGSAP } from '@gsap/react';
+gsap.registerPlugin(useGSAP);
+
+
 
 function App() {
   const [count, setCount] = useState(0)
   useGSAP(()=>{
-    gsap.from()
-  })
+    // gsap.from()
+  },[]);
   
 
   return (
-    <>
-      <div id = "red-box" className="bg-red-600 w-20 h-20"></div>    
-    </>
+    
+      <>
+        <div id="red-box" className="bg-red-600 w-20 h-20">123</div>
+        <div className="">123</div>
+      </>
   )
 }
 
