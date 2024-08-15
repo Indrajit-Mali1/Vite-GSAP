ba2e66ae459feda9424a55c353421ac432cb7182
Indrajit Mali <142807827+Indrajit-Mali1@users.noreply.github.com>
2024-08-15 03:26:05 +0000

    modified:   GSAP/src/App.css 	modified:   GSAP/src/App.jsx 	modified:   GSAP/src/index.css

--- GSAP/src/App.jsx
+++ GSAP/src/App.jsx
@@ -11,20 +11,37 @@ gsap.registerPlugin(useGSAP);
 
 function App() {
   const [count, setCount] = useState(0)
-  useGSAP(()=>{
-    // gsap.from()
-  },[]);
-  useGSAP(()=>{
-
+  useGSAP(() => {
+    gsap.fromTo('#red-box', {
+      x: 0,
+      rotation: 0,
+      borderRadius: "0%"
+    }, {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      borderRadius: '100%',
+      rotation: 360,
+      duration: 2,
+      ease: 'bounce.out'
+    })
   }, []);
-    
+
+
 
   return (
-    
-      <>
-        <div id="red-box" className="bg-red-600 w-20 h-20">123</div>
-        <div className="">123</div>
-      </>
+
+    <>
+      <main className='bg-black w-screen '>
+        <div id="red-box" className="bg-red-600 w-20 h-20"></div>
+        <div>
+          <button onClick={() => { }}>Play/Pause</button>
+          <div id="yellow-box" className="bg-yellow-500 w-20 h-20"></div>
+        </div>
+      </main>
+
+      <div className="">123</div>
+    </>
   )
 }
 
